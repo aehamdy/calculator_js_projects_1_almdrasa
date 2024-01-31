ff687d5780374766d31d4c6b14e28f07a861efc0
aehamdy <ae.hamdy91@gmail.com>
2024-01-31 14:16:59 +0200

    Added numbers to screen

--- script.js
+++ script.js
@@ -7,3 +7,32 @@ const toggleDarkThemeByEnter = (event) => event.key === "Enter" && toggleDarkThe
 
 themeElement.addEventListener("keydown", toggleDarkThemeByEnter);
 themeElement.addEventListener("click", toggleDarkTheme);
+
+// Logic for calculator
+
+let storedNumber = "";
+let currentNumber = "";
+let operation = "";
+
+const resultElement = document.querySelector(".calc__result");
+const keyElements = document.querySelectorAll("[data-type]");
+
+const updateScreen = (value) => {
+    resultElement.innerText = !value ? "0" : value;
+};
+
+const numberButtonHandler = (value) => {
+    if (value === "." && currentNumber.includes(".")) return;
+    if (value === "0" && !currentNumber) return;
+
+    currentNumber += value;
+    updateScreen(currentNumber);
+};
+
+const keyElementsHandler = (element) => {
+    element.addEventListener("click", () => {
+        element.dataset.type === "number" && numberButtonHandler(element.dataset.value);
+    });
+};
+
+keyElements.forEach(keyElementsHandler);
